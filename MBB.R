d656d39cf8bee603f39d3ea080af983e7ac1a367
Yunhua Xiang <xiangyh@uw.edu>
2016-09-21 22:16:26 -0700

    Add files via upload

---
+++ MBB.R
@@ -0,0 +1,31 @@
+
+##=============================
+##   Moving Block Bootstrap
+##=============================
+
+# simulate data
+n <- 100                               
+series <- arima.sim(model=list(ar=0.2),n)                 
+l <- 6                                  
+B <- 1000                            
+
+stat.Mbt <- rep(NA,B) 
+k <- floor(n/l) # size
+
+# the bootstrap loop
+for(irep in 1:B) {                   
+  sbt <- rep(NA,k*l)     
+  
+  # random blocks
+  for(i in 1:k) {           
+    beginpoint <- sample(1:(n-l+1), size=1)     # by randomly sampling beginpoints
+    sbt[(i-1)*l+1:l] <- ts[beginpoint+0:(l-1)] # and copying blocks
+  }                                 # sbt has kl <= l elements.
+  stat.Mbt[irep] <- mean(sbt)  # the autocorrlation estimate
+}
+
+# > mean(stat.Mbt)
+# [1] -0.2128445
+# > sd(stat.Mbt)
+# [1] 0.1324477
+# > hist(stat.Mbt)
