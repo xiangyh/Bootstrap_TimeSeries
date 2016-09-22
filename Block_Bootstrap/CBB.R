6c10da6a75409825a6afaa2f02c3ace2cd6fdb76
Yunhua Xiang <xiangyh@uw.edu>
2016-09-21 22:26:57 -0700

    Add files via upload

---
+++ Block_Bootstrap/CBB.R
@@ -0,0 +1,29 @@
+##=============================
+##   Circular Block Bootstrap
+##=============================
+
+
+# simulate data
+n <- 120                               
+series <- arima.sim(model=list(ar=0.2),n)                 
+l <- 4                                 
+B <- 1000          
+
+stat.Cbt <- rep(NA,B) 
+k <- floor(n/l)
+# vessel for the boostrapped values
+series.y <- rep(series,2)
+for(irep in 1:B) {                   # the bootstrap loop
+  sbt <- rep(NA,k*l)                #  bootstrap replication
+  for(i in 1:k) {            # random blocks
+    beginpoint <- sample(1:n, size=1)     # sampling beginpoints
+    sbt[(i-1)*l+1:l] <- series.y[beginpoint+0:(l-1)] # and copying blocks
+  }                          # sbt has kl elements
+  stat.Cbt[irep] <- stat.fun(sbt)  
+}
+
+
+# > mean(stat.Cbt)
+# [1] -0.04355332
+# > sd(stat.Cbt)
+# [1] 0.1204954
